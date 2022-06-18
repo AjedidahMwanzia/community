d8fe05f52885e0e0bf3cd23bcf2e289d90c4c105
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-19 00:30:05 +0300

    meadia and static configurations

--- community/settings.py
+++ community/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'community.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'community',
+        'USER': ,
+        'PASSWORD': ,
     }
 }
 
@@ -117,6 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='/images/'
+
+STATICFILES_DIRS=[
+    BASE_DIR /'static'
+]
+
+MEDIA_ROOT= BASE_DIR/'static/images'
+STATIC_ROOT = BASE_DIR/'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

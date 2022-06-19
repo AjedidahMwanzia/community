b6cc8187d2f6997924d31cb1c31ab40b6ba8d105
AjedidahMwanzia <ajedidah.mwanzia@student.moringaschool.com>
2022-06-19 08:58:32 +0300

    add profile,post and comments in models.py

--- watch/models.py
+++ watch/models.py
@@ -49,3 +49,80 @@ class Business(models.Model):
         biz = cls.objects.filter(name__icontains=search_term)
         return biz
 
+class Profile(models.Model):
+    Profile_photo = CloudinaryField("image")
+    Bio = models.TextField(max_length = 50,null = True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE, primary_key=True)
+    neighborhood = models.ForeignKey('Neighborhood', related_name='neighbourhood',null=True)
+    email_address = models.CharField(max_length = 50,null = True)
+    business = models.ForeignKey('Business',related_name='business',null=True)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_by_id(cls, id):
+        details = Profile.objects.get(user = id)
+        return details
+
+    @classmethod
+    def filter_by_id(cls, id):
+        details = Profile.objects.filter(user = id).first()
+        return details
+    
+    @classmethod
+    def search_user(cls, name):
+        userprof = Profile.objects.filter(user__username__icontains = name)
+        return userprof
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Post(models.Model):
+    name = models.CharField(max_length=50,blank=True)
+    image = CloudinaryField("image")
+    description = models.TextField(max_length = 50,null = True)
+    user = models.ForeignKey(User, null = True,related_name='post')
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    neighborHood = models.ForeignKey(NeighborHood, null = True,related_name='posts')
+    
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def get_hood_posts(cls,id):
+        posts = Post.objects.filter(id = id)
+        return posts
+
+class Comment(models.Model):
+    name = models.CharField(max_length=30)
+    post = models.ForeignKey(Post,null = True)
+    neighborhood = models.ForeignKey(NeighborHood,related_name='comment')
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-id']
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def find_commentpost(cls,id):
+        comments = Comments.objects.filter(post__pk = id)
+        return comments
